341580615bd9de639828a2c6cd0dcb32340d6234
Bountyhunter <ppppanup25@gmail.com>
2026-09-17 20:50:08 +0530

    arithmetic operations done

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+a = 10
+b = 3
+print("Addition:", a + b) # This line will print "Addition: 13" because it adds the values of a and b.
+print("Subtraction:", a - b) # This line will print "Subtraction: 7" because it subtracts the value of b from a.
+print("Multiplication:", a * b) # This line will print "Multiplication: 30" because it multiplies the values of a and b.
+print("Division:", a / b) # This line will print "Division: 3.3333333333333335" because it divides the value of a by b.
+print("Floor Division:", a // b) # This line will print "Floor Division: 3" because it performs floor division on a and b.
+print("Modulus:", a % b) # This line will print "Modulus: 1" because it calculates the remainder of the division of a by b.
+print("Exponentiation:", a ** b) # This line will print "Exponentiation: 1000" because it raises a to the power of b.
+print("hi" * 3)   # This line will print "hihihi" because the string "hi" is repeated 3 times.

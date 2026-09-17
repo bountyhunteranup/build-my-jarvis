789b865edb900ae999d967b15111391dfa633c45
Bountyhunter <ppppanup25@gmail.com>
2026-09-17 08:09:51 +0530

    Add Data Types

---
+++ data_types.py
@@ -0,0 +1,14 @@
+name = "Anup"
+age = 20
+height = 1.75
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))

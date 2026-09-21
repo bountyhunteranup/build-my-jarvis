fedf21ef0697048aaf6ec74c579193ab2ad79eeb
Bountyhunter <ppppanup25@gmail.com>
2026-09-21 16:32:13 +0530

    Age calculator added

---
+++ age_cal.py
@@ -0,0 +1,8 @@
+age = int(input("Please enter your age: "))
+
+print("\n===== AGE CALCULATOR =====\n")
+print(f"Current Age: {age}")
+print(f"Age after 1 year: {age + 1}")
+print(f"Age after 5 years: {age + 5}")
+print(f"Age after 10 years: {age + 10}")
+print("\n=============================\n")

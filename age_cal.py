529bbc6a86ddadc8a52fe908a5a225b6302a7734
Bountyhunter <ppppanup25@gmail.com>
2026-09-26 14:58:35 +0530

    age calculator modified

--- age_cal.py
+++ age_cal.py
@@ -1,8 +1,15 @@
-age = int(input("Please enter your age: "))
-
-print("\n===== AGE CALCULATOR =====\n")
-print(f"Current Age: {age}")
-print(f"Age after 1 year: {age + 1}")
-print(f"Age after 5 years: {age + 5}")
-print(f"Age after 10 years: {age + 10}")
-print("\n=============================\n")
+def age_cal():
+    age = input("Please enter your age: ")
+    if age == "" :
+        print("Please enter a valid age. Age cannot be negative or zero, you can run the program again.")
+        age_cal()
+    else :
+        age1 = int(age)
+        print("\n===== AGE CALCULATOR =====\n")
+        print(f"Current Age: {age1}")
+        print(f"Age after 1 year: {age1 + 1}")
+        print(f"Age after 5 years: {age1 + 5}")
+        print(f"Age after 10 years: {age1 + 10}")
+        print("\n=============================\n")
+        exit()
+age_cal()

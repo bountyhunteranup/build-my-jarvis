7a0c5c72df8e0d6aa87ace0fdf72bde49e1fcf9b
Bountyhunter <ppppanup25@gmail.com>
2026-09-26 14:35:05 +0530

    bmi calculator code has been updated

--- bmi_cal.py
+++ bmi_cal.py
@@ -7,9 +7,9 @@ print(f"Your BMI is: {bmi}")
 
 if bmi < 18.5:
     print("You are underweight.")
-elif 18.5 < bmi < 24.9:
+elif bmi < 24.9:
     print("You have a normal weight.")
-elif 25 < bmi < 29.9:
+elif bmi < 29.9:
     print("You are overweight.")
 elif bmi > 30:
     print("You are obese.")
